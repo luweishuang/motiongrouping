a6b921fc883a8ca11d95687b0e291541065ad4f2
luweishuang <pifuchan@gmail.com>
2021-06-04 16:35:36 +0800

    debug infer

--- raft/run_inference.py
+++ raft/run_inference.py
@@ -7,17 +7,23 @@ reverse = [0, 1]
 rgbpath = os.path.join(data_path, 'JPEGImages/1080p')  # path to the dataset
 folder = gb.glob(os.path.join(rgbpath, '*'))
 
+cnt = 0
 for r in reverse:
-  for g in gap:
-    for f in folder:
-      print('===> Runing {}, gap {}'.format(f, g))
-      mode = 'raft-things.pth'  # model
-      if r==1:
-        raw_outroot = data_path + '/Flows_gap-{}/'.format(g)  # where to raw flow
-        outroot = data_path + '/FlowImages_gap-{}/'.format(g)  # where to save the image flow
-      elif r==0:
-        raw_outroot = data_path + '/Flows_gap{}/'.format(g)   # where to raw flow
-        outroot = data_path + '/FlowImages_gap{}/'.format(g)   # where to save the image flow
-      os.system("python predict.py "
-                "--gap {} --mode {} --path {} "
-                "--outroot {} --reverse {} --raw_outroot {}".format(g, mode, f, outroot, r, raw_outroot))
+    for g in gap:
+        for f in folder:
+            # print('===> Runing {}, gap {}'.format(f, g))
+            mode = 'raft-things.pth'  # model
+            if r==1:
+                raw_outroot = data_path + '/Flows_gap-{}/'.format(g)  # where to raw flow
+                outroot = data_path + '/FlowImages_gap-{}/'.format(g)  # where to save the image flow
+            elif r==0:
+                raw_outroot = data_path + '/Flows_gap{}/'.format(g)   # where to raw flow
+                outroot = data_path + '/FlowImages_gap{}/'.format(g)   # where to save the image flow
+            print("python predict.py "
+                    "--gap {} --model {} --path {} "
+                    "--outroot {} --reverse {} --raw_outroot {}".format(g, mode, f, outroot, r, raw_outroot))
+            os.system("python predict.py "
+                    "--gap {} --model {} --path {} "
+                    "--outroot {} --reverse {} --raw_outroot {}".format(g, mode, f, outroot, r, raw_outroot))
+            cnt += 1
+print(cnt)
